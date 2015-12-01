8ed3f5a7c995da960956fbfef6af2f0d3165e7d6
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2015-12-01 09:54:35 +0400

    Add nullable modifier.

--- Pod/Classes/Serialization/JSONSerializationLifecycleProtocol.h
+++ Pod/Classes/Serialization/JSONSerializationLifecycleProtocol.h
@@ -16,7 +16,7 @@
  Should return a dictionary with format @{"PROPERTY NAME" : TRANSFORMER}
  */
 + (nonnull NSDictionary *)modelTransformers;
-+ (nonnull NSDictionary *)modelTransformersWithUserInfo:(NSDictionary *)userInfo;
++ (nonnull NSDictionary *)modelTransformersWithUserInfo:(nullable NSDictionary *)userInfo;
 + (nullable NSDictionary *)keysForKeyPaths:(nullable NSDictionary *)userInfo;
 
 /**
