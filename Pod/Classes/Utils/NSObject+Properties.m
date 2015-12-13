be43ddead87101ec6ffe409e3795619a4fcc67db
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2015-12-13 16:18:15 +0400

    Fix simple properties fetching.

--- Pod/Classes/Utils/NSObject+Properties.m
+++ Pod/Classes/Utils/NSObject+Properties.m
@@ -38,30 +38,35 @@ static void *FJCachedPropertyKeysKey = &FJCachedPropertyKeysKey;
 #pragma mark - Utils
 
 + (NSDictionary *)keysWithProperties:(NSSet *)properties {
-    return [self keysWithProperties:properties sneak:NO];
+    return [self keysWithProperties:properties prefix:NO sneak:NO];
 }
 
 + (NSDictionary *)keysWithProperties:(NSSet *)properties sneak:(BOOL)sneak {
+    return [self keysWithProperties:properties prefix:YES sneak:sneak];
+}
+
++ (NSDictionary *)keysWithProperties:(NSSet *)properties prefix:(BOOL)prefix sneak:(BOOL)sneak {
     NSMutableDictionary *keys = [NSMutableDictionary dictionary];
     NSError *error = nil;
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"([a-z])([A-Z])" options:0 error:&error];
     NSString *identifierString = [[[FluentJConfiguration sharedInstance] identifierKeyPathName] lowercaseString];
     for(FJPropertyDescriptor *propertyDescriptor in properties) {
         NSString *value = propertyDescriptor.name;
-        if(sneak) {
-            NSRange range = NSMakeRange(0, [propertyDescriptor.name length]);
-            value = [[regex stringByReplacingMatchesInString:propertyDescriptor.name options:0 range:range withTemplate:@"$1_$2"] lowercaseString];
-            if(!FJSimpleClass(propertyDescriptor.typeClass)) {
-                value = [NSString stringWithFormat:@"%@_%@", value, identifierString];
-            }
-        } else {
-            if(!FJSimpleClass(propertyDescriptor.typeClass)) {
-                value = [NSString stringWithFormat:@"%@%@", value, [identifierString capitalizedStringWithIndex:0]];
+        if(prefix) {
+            if(sneak) {
+                NSRange range = NSMakeRange(0, [propertyDescriptor.name length]);
+                value = [[regex stringByReplacingMatchesInString:propertyDescriptor.name options:0 range:range withTemplate:@"$1_$2"] lowercaseString];
+                if(!FJSimpleClass(propertyDescriptor.typeClass)) {
+                    value = [NSString stringWithFormat:@"%@_%@", value, identifierString];
+                }
+            } else {
+                if(!FJSimpleClass(propertyDescriptor.typeClass)) {
+                    value = [NSString stringWithFormat:@"%@%@", value, [identifierString capitalizedStringWithIndex:0]];
+                }
             }
         }
         [keys setValue:value forKey:propertyDescriptor.name];
     }
     return keys;
 }
-
 @end
