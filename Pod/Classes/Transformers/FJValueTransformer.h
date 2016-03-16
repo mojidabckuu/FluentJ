76087297124b5b2b4b54aa94812174f3bb4605d3
Vlad Gorbenko <mojidabckuu.22.06.92@gmail.com>
2016-03-16 10:48:57 +0400

    add nullable specs.

--- Pod/Classes/Transformers/FJValueTransformer.h
+++ Pod/Classes/Transformers/FJValueTransformer.h
@@ -8,20 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
-typedef id (^FJValueTransformerBlock)(id value, BOOL *success, NSError **error);
-typedef id (^FJValueTransformBlock)(id value, NSDictionary<NSString *, id> *userInfo, NSError **error);
+typedef __nullable id (^FJValueTransformerBlock)(id _Nullable value, BOOL * _Nonnull success, NSError * _Nullable * _Nullable error);
+typedef __nullable id (^FJValueTransformBlock)(id _Nullable value, NSDictionary<NSString *, id> * _Nonnull userInfo, NSError * _Nullable * _Nullable error);
 
 @interface FJValueTransformer : NSValueTransformer
 
-@property (nonatomic, strong, readonly) NSDictionary *userInfo;
+@property (nonnull, nonatomic, strong, readonly) NSDictionary *userInfo;
 
-+ (instancetype)transformerUsingForwardBlock:(FJValueTransformerBlock)transformation  __attribute__ ((deprecated));
-+ (instancetype)transformerUsingForwardBlock:(FJValueTransformBlock)transformation userInfo:(NSDictionary<NSString *, id> *)userInfo;
++ (nonnull instancetype)transformerUsingForwardBlock:(nonnull FJValueTransformerBlock)transformation  __attribute__ ((deprecated));
++ (nonnull instancetype)transformerUsingForwardBlock:(_Nonnull FJValueTransformBlock)transformation userInfo:(nullable NSDictionary<NSString *, id> *)userInfo;
 
-+ (instancetype)transformerUsingReversibleBlock:(FJValueTransformerBlock)transformation  __attribute__ ((deprecated));
-+ (instancetype)transformerUsingReversibleBlock:(FJValueTransformBlock)transformation userInfo:(NSDictionary<NSString *, id> *)userInfo;
++ (nonnull instancetype)transformerUsingReversibleBlock:(nonnull FJValueTransformerBlock)transformation  __attribute__ ((deprecated));
++ (nonnull instancetype)transformerUsingReversibleBlock:(nonnull FJValueTransformBlock)transformation userInfo:(nullable NSDictionary<NSString *, id> *)userInfo;
 
-+ (instancetype)transformerUsingForwardBlock:(FJValueTransformerBlock)forwardTransformation reverseBlock:(FJValueTransformerBlock)reverseTransformation  __attribute__ ((deprecated));
-+ (instancetype)transformerUsingForwardBlock:(FJValueTransformBlock)forwardTransformation reverseBlock:(FJValueTransformBlock)reverseTransformation userInfo:(NSDictionary<NSString *, id> *)userInfo;
++ (nonnull instancetype)transformerUsingForwardBlock:(nonnull FJValueTransformerBlock)forwardTransformation reverseBlock:(nonnull FJValueTransformerBlock)reverseTransformation  __attribute__ ((deprecated));
++ (nonnull instancetype)transformerUsingForwardBlock:(nonnull FJValueTransformBlock)forwardTransformation reverseBlock:(nonnull FJValueTransformBlock)reverseTransformation userInfo:(nullable NSDictionary<NSString *, id> *)userInfo;
 
 @end
--- Pod/Classes/Transformers/FJValueTransformer.m
+++ Pod/Classes/Transformers/FJValueTransformer.m
@@ -28,18 +28,18 @@
 #pragma mark - Lifecycle
 
 + (instancetype)transformerUsingForwardBlock:(FJValueTransformBlock)forwardBlock userInfo:(NSDictionary *)userInfo {
-    return [[self alloc] initWithForwardBlock:forwardBlock reverseBlock:nil userInfo:userInfo];
+    return [[FJReversibleValueTransformer alloc] initWithForwardBlock:forwardBlock reverseBlock:nil userInfo:userInfo];
 }
 
 + (instancetype)transformerUsingReversibleBlock:(FJValueTransformBlock)reversibleBlock userInfo:(NSDictionary *)userInfo {
-    return [self transformerUsingForwardBlock:nil reverseBlock:reversibleBlock userInfo:userInfo];
+    return [[FJReversibleValueTransformer alloc] initWithForwardBlock:nil reverseBlock:reversibleBlock userInfo:userInfo];
 }
 
 + (instancetype)transformerUsingForwardBlock:(FJValueTransformBlock)forwardBlock reverseBlock:(FJValueTransformBlock)reverseBlock userInfo:(NSDictionary *)userInfo {
     return [[FJReversibleValueTransformer alloc] initWithForwardBlock:forwardBlock reverseBlock:reverseBlock userInfo:userInfo];
 }
 
-- (id)initWithForwardBlock:(FJValueTransformBlock)forwardBlock reverseBlock:(FJValueTransformBlock)reverseBlock userInfo:(NSDictionary *)userInfo {
+- (id)initWithForwardBlock:(nullable FJValueTransformBlock)forwardBlock reverseBlock:(nullable FJValueTransformBlock)reverseBlock userInfo:(NSDictionary *)userInfo {
     NSParameterAssert(forwardBlock != nil);
     
     self = [super init];
